78039cd6e406a3ee461270886c6123a9c9bc107c
Alvin Samuel Jeyachander <39959076+AlvinJeyachander@users.noreply.github.com>
2023-02-08 18:06:54 -0500

    Add files via upload

---
+++ SQLQuery_1.sql
@@ -0,0 +1,23 @@
+Select * From Nas_Ho_Data_CL;
+--Populate property address data
+Select * 
+From Nas_Ho_Data_CL
+WHERE PropertyAddress IS NULL;
+
+Select  a.UniqueID,a.ParcelID,a.PropertyAddress,b.UniqueID, b.PropertyAddress,ISNULL(a.PropertyAddress,b.PropertyAddress)
+From Nas_Ho_Data_CL a
+JOIN Nas_Ho_Data_CL b
+on a.ParcelID=b.ParcelID
+AND a.UniqueID!=b.UniqueID
+WHERE a.PropertyAddress IS NULL;
+---
+UPDATE a
+SET a.PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
+From Nas_Ho_Data_CL a
+JOIN Nas_Ho_Data_CL b
+on a.ParcelID=b.ParcelID
+AND a.UniqueID!=b.UniqueID
+WHERE a.PropertyAddress IS NULL;
+----
+
+
